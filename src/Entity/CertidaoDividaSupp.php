f3456af200cc48229056dc40d094f59784488829
danilopbh <danilo.dlima@pbh.gov.br>
2024-10-10 11:24:14 -0300

    entidade cda e contribuinte

---
+++ src/Entity/CertidaoDividaSupp.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CertidaoDividaSuppRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CertidaoDividaSuppRepository::class)]
+class CertidaoDividaSupp
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?ContribuinteSupp $id_contribuinte_supp = null;
+
+    #[ORM\Column]
+    private ?float $valor = null;
+
+    #[ORM\Column(type: Types::BLOB)]
+    private $pdfdivida;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $descricao = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $data_vencimento = null;
+
+    #[ORM\ManyToOne(inversedBy: 'certidaoDividaSupps')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?ContribuinteSupp $contribuinte_supp = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getIdContribuinteSupp(): ?ContribuinteSupp
+    {
+        return $this->id_contribuinte_supp;
+    }
+
+    public function setIdContribuinteSupp(?ContribuinteSupp $id_contribuinte_supp): static
+    {
+        $this->id_contribuinte_supp = $id_contribuinte_supp;
+
+        return $this;
+    }
+
+    public function getValor(): ?float
+    {
+        return $this->valor;
+    }
+
+    public function setValor(float $valor): static
+    {
+        $this->valor = $valor;
+
+        return $this;
+    }
+
+    public function getPdfdivida()
+    {
+        return $this->pdfdivida;
+    }
+
+    public function setPdfdivida($pdfdivida): static
+    {
+        $this->pdfdivida = $pdfdivida;
+
+        return $this;
+    }
+
+    public function getDescricao(): ?string
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao(?string $descricao): static
+    {
+        $this->descricao = $descricao;
+
+        return $this;
+    }
+
+    public function getDataVencimento(): ?\DateTimeInterface
+    {
+        return $this->data_vencimento;
+    }
+
+    public function setDataVencimento(\DateTimeInterface $data_vencimento): static
+    {
+        $this->data_vencimento = $data_vencimento;
+
+        return $this;
+    }
+
+    public function getContribuinteSupp(): ?ContribuinteSupp
+    {
+        return $this->contribuinte_supp;
+    }
+
+    public function setContribuinteSupp(?ContribuinteSupp $contribuinte_supp): static
+    {
+        $this->contribuinte_supp = $contribuinte_supp;
+
+        return $this;
+    }
+}
---
+++ src/Entity/ContribuinteSupp.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContribuinteSuppRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ContribuinteSuppRepository::class)]
+class ContribuinteSupp
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nome = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $cpf = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $endereco = null;
+
+    /**
+     * @var Collection<int, CertidaoDividaSupp>
+     */
+    #[ORM\OneToMany(targetEntity: CertidaoDividaSupp::class, mappedBy: 'contribuinte_siatu')]
+    private Collection $certidaoDividaSupps;
+
+    public function __construct()
+    {
+        $this->certidaoDividaSupps = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): static
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getCpf(): ?string
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf(string $cpf): static
+    {
+        $this->cpf = $cpf;
+
+        return $this;
+    }
+
+    public function getEndereco(): ?string
+    {
+        return $this->endereco;
+    }
+
+    public function setEndereco(string $endereco): static
+    {
+        $this->endereco = $endereco;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, CertidaoDividaSupp>
+     */
+    public function getCertidaoDividaSupps(): Collection
+    {
+        return $this->certidaoDividaSupps;
+    }
+
+    public function addCertidaoDividaSupp(CertidaoDividaSupp $certidaoDividaSupp): static
+    {
+        if (!$this->certidaoDividaSupps->contains($certidaoDividaSupp)) {
+            $this->certidaoDividaSupps->add($certidaoDividaSupp);
+            $certidaoDividaSupp->setContribuinteSupp($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCertidaoDividaSupp(CertidaoDividaSupp $certidaoDividaSupp): static
+    {
+        if ($this->certidaoDividaSupps->removeElement($certidaoDividaSupp)) {
+            // set the owning side to null (unless already changed)
+            if ($certidaoDividaSupp->getContribuinteSupp() === $this) {
+                $certidaoDividaSupp->setContribuinteSupp(null);
+            }
+        }
+
+        return $this;
+    }
+}

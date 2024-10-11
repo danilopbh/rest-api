8451a2bf543da59ceab9976d2731c2239f2641c9
danilopbh <danilo.dlima@pbh.gov.br>
2024-10-11 09:07:45 -0300

    inicio controller

--- src/Controller/ContribuinteSiatuController.php
+++ src/Controller/ContribuinteSiatuController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ContribuinteSiatuRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContribuinteSiatuController extends AbstractController
 {
@@ -15,5 +17,44 @@ class ContribuinteSiatuController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/ContribuinteSiatuController.php',
         ]);
+    }
+
+
+    #[Route('/api/contribuinte/siatu', name: 'app_contribuintes_siatu', methods: ['GET'])]
+    public function getContribuintesSiatu(ContribuinteSiatuRepository $contribuinteSiatuRepository): JsonResponse
+    {
+        
+       
+        // Fetch all Contribuintes with their CertidaoDivida relations
+        $contribuintes = $contribuinteSiatuRepository->findAll();
+        
+       dd($contribuintes);
+      
+
+        $data = [];
+
+        foreach ($contribuintes as $contribuinte) {
+            $certidoes = [];
+            
+            foreach ($contribuinte->getCertidoesDividaSiatu() as $certidaoDivida) {
+
+                
+                $certidoes[] = [
+                    'id' => $certidaoDivida->getId(),
+                    'descricao' => $certidaoDivida->getDescricao(),
+                    'dataEmissao' => $certidaoDivida->getDataEmissao()->format('Y-m-d'),
+                    'valor' => $certidaoDivida->getValor(),
+                ];
+            }
+
+            $data[] = [
+                'id' => $contribuinte->getId(),
+                'nome' => $contribuinte->getNome(),
+                'cpf' => $contribuinte->getCpf(),
+                'certidoesDivida' => $certidoes,
+            ];
+        }
+
+        return new JsonResponse($data, Response::HTTP_OK);
     }
 }
